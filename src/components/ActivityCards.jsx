97b8db69dddfbfc852770ac3a08bdc21af06f7ad
Jusiah <71131010+jusiahjusiah@users.noreply.github.com>
2023-02-01 20:19:38 +0800

    bug-fix-on-activity-cards

--- src/components/ActivityCards.jsx
+++ src/components/ActivityCards.jsx
@@ -12,10 +12,10 @@ const ActivityCards = (props) => {
                     <p className='activity-rating-icon'> ★ </p>
                     <p className='activity-rating'> {props.stats.rating} </p>
                     <p className='activity-rating-count'>  ({props.stats.reviewCount}) </p>
-                    <p className='activity-rating-country'> - {props.country}  </p>
+                    <p className='activity-rating-country'> - {props.location}  </p>
                 </div>
                     <p className='activity-title'>{props.title} </p>
-                    <p className='activity-pricing'> <b> From ${props.pricing} </b> / person </p>
+                    <p className='activity-pricing'> <b> From ${props.price} </b> / person </p>
             </div>
     )
 }
